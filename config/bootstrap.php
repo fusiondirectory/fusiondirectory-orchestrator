f65bd13c8600278d4da3ab92f3081cf2e9510576
Thibault Dockx <thibault.dockx@fusiondirectory.org>
2023-01-10 16:50:04 +0000

    :sparkles: Feat(Bootstrap) - Finalize the autoloader.
    Autoloader made production ready.
    Comments added.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,11 +1,46 @@
 <?php
 
-require dirname(__DIR__) . "/vendor/autoload.php";
+/*
+ * Allows to autoload php system libraries required by the code.
+ * Allows to load the 3 dependencies require by php-mailer.
+ * Allows to iterate on parent directory for any required class.
+ */
+function autoload ($class)
+{
+  if (strpos($class, 'PHPMailer') !== FALSE) {
+    require("/usr/share/php/libphp-phpmailer/src/Exception.php");
+    require("/usr/share/php/libphp-phpmailer/src/PHPMailer.php");
+    require("/usr/share/php/libphp-phpmailer/src/SMTP.php");
+  }
+
+    $relative_class = str_replace('\\', '/', $class) . '.php';
+    $base_dirs = ['/usr/share/php', dirname(__DIR__)];
+    $files = [];
+  foreach ($base_dirs as $base_dir) {
+      $dir = new RecursiveDirectoryIterator($base_dir);
+      $iter = new RecursiveIteratorIterator($dir);
+      $regex = new RegexIterator($iter, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+
+    foreach ($regex as $file) {
+      $files[] = $file[0];
+    }
+  }
+
+  foreach ($files as $file) {
+    if (strpos($file, $relative_class) !== FALSE) {
+
+      require $file;
+      break;
+    }
+  }
+}
+
+spl_autoload_register('autoload');
 
 set_error_handler("ErrorHandler::handleError");
 set_exception_handler("ErrorHandler::handleException");
 
-$dotenv = Dotenv\Dotenv::createImmutable('/etc/orchestrator/', 'orchestrator.conf');
+$dotenv = Dotenv\Dotenv::create('/etc/orchestrator', 'orchestrator.conf');
 $dotenv->load();
 
 header("Content-type: application/json; charset=UTF-8");
