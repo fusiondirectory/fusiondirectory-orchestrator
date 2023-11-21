f5d0136c3c9b3a2706642231c07dd702a0bfa998
Thibault Dockx <thibault.dockx@fusiondirectory.org>
2023-11-21 17:15:06 +0000

    :sparkles: Feat(orchestrator) - Read receipt working
    Read receipt are now functional.

--- library/MailController.php
+++ library/MailController.php
@@ -1,107 +1,112 @@
-<?php
+  <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
 
-class MailController
-{
-
-  protected string $setFrom;
-  protected ?string $replyTo;
-  protected array $recipients;
-  protected string $body;
-  protected ?string $signature;
-  protected string $subject;
-  protected ?bool $receipt;
-  protected ?array $attachments;
-  private PHPMailer $mail;
-
-  function __construct (
-    string $setFrom,
-    ?string $replyTo,
-    array $recipients,
-    string $body,
-    ?string $signature,
-    string $subject,
-    bool $receipt      = NULL,
-    array $attachments = NULL
-  )
+  class MailController
   {
-    // The TRUE value passed it to enable the exception handling properly.
-    $this->mail = new PHPMailer(TRUE);
 
-    $this->setFrom     = $setFrom;
-    $this->replyTo     = $replyTo;
-    $this->recipients  = $recipients;
-    $this->body        = $body;
-    $this->signature   = $signature;
-    $this->subject     = $subject;
-    $this->receipt     = $receipt;
-    $this->attachments = $attachments;
+    protected string $setFrom;
+    protected ?string $replyTo;
+    protected array $recipients;
+    protected string $body;
+    protected ?string $signature;
+    protected string $subject;
+    protected ?bool $receipt;
+    protected ?array $attachments;
+    private PHPMailer $mail;
 
-  }
+    function __construct (
+      string $setFrom,
+      ?string $replyTo,
+      array $recipients,
+      string $body,
+      ?string $signature,
+      string $subject,
+      bool $receipt      = NULL,
+      array $attachments = NULL
+    )
+    {
+      // The TRUE value passed it to enable the exception handling properly.
+      $this->mail = new PHPMailer(TRUE);
 
-  public function sendMail () : array
-  {
-    $this->mail->isSMTP();
-    $this->mail->Host = $_ENV["MAIL_HOST"];
+      $this->setFrom     = $setFrom;
+      $this->replyTo     = $replyTo;
+      $this->recipients  = $recipients;
+      $this->body        = $body;
+      $this->signature   = $signature;
+      $this->subject     = $subject;
+      $this->receipt     = $receipt;
+      $this->attachments = $attachments;
 
-    /*
-     * In case there are FQDN errors responses by the SMTP server, try below.
-     * $this->mail->Helo = '['.$_SERVER['SERVER_ADDR'].']';
-     */
-
-    $this->mail->SMTPAuth   = TRUE;
-    $this->mail->Username   = $_ENV["MAIL_USER"];
-    $this->mail->Password   = $_ENV["MAIL_PASS"];
-    $this->mail->SMTPSecure = $_ENV["MAIL_SEC"];
-    $this->mail->Port       = $_ENV["MAIL_PORT"];
-    $this->mail->AuthType   = 'LOGIN';
-
-    foreach($this->attachments as $attachment) {
-      $this->mail->addStringAttachment($attachment['content'], $attachment['cn']);
     }
 
-    $this->mail->setFrom($this->setFrom);
+    public function sendMail () : array
+    {
+      $this->mail->isSMTP();
+      $this->mail->Host = $_ENV["MAIL_HOST"];
 
-    if (!empty($this->replyTo)) {
-      $this->mail->addReplyTo($this->replyTo);
-    }
+      /*
+       * In case there are FQDN errors responses by the SMTP server, try below.
+       * $this->mail->Helo = '['.$_SERVER['SERVER_ADDR'].']';
+       */
 
-    $this->mail->Subject = $this->subject;
-    $this->mail->Body    = $this->body;
+      $this->mail->SMTPAuth   = TRUE;
+      $this->mail->Username   = $_ENV["MAIL_USER"];
+      $this->mail->Password   = $_ENV["MAIL_PASS"];
+      $this->mail->SMTPSecure = $_ENV["MAIL_SEC"];
+      $this->mail->Port       = $_ENV["MAIL_PORT"];
+      $this->mail->AuthType   = 'LOGIN';
 
-    if (!empty($this->signature)) {
-      $this->mail->Body .= "\n\n". $this->signature;
-    }
-
-    // add it to keep SMTP connection open after each email sent
-    $this->mail->SMTPKeepAlive = TRUE;
-
-    unset($this->recipients["count"]);
-
-    // Our returned array
-    $errors = [];
-
-    foreach ($this->recipients as $mail) {
-      $this->mail->addAddress($mail);
-
-      try {
-        $this->mail->send();
-
-      } catch (Exception $e) {
-        $errors[] = $this->mail->ErrorInfo;
-
+      if (!empty($this->attachments)) {
+        foreach($this->attachments as $attachment) {
+          $this->mail->addStringAttachment($attachment['content'], $attachment['cn']);
+        }
       }
-      $this->mail->clearAddresses();
 
-      if (empty($errors)) {
-        $errors[] = "SUCCESS";
+      $this->mail->setFrom($this->setFrom);
+
+      if (!empty($this->replyTo)) {
+        $this->mail->addReplyTo($this->replyTo);
       }
-    }
 
-    $this->mail->smtpClose();
-    return $errors;
+      if (!empty($this->receipt)) {
+        $this->mail->addCustomHeader('Disposition-Notification-To', $this->setFrom);
+      }
+      $this->mail->Subject = $this->subject;
+      $this->mail->Body    = $this->body;
+
+      if (!empty($this->signature)) {
+        $this->mail->Body .= "\n\n". $this->signature;
+      }
+
+      // add it to keep SMTP connection open after each email sent
+      $this->mail->SMTPKeepAlive = TRUE;
+
+      unset($this->recipients["count"]);
+
+      // Our returned array
+      $errors = [];
+
+      foreach ($this->recipients as $mail) {
+        $this->mail->addAddress($mail);
+
+        try {
+          $this->mail->send();
+
+        } catch (Exception $e) {
+          $errors[] = $this->mail->ErrorInfo;
+
+        }
+        $this->mail->clearAddresses();
+
+        if (empty($errors)) {
+          $errors[] = "SUCCESS";
+        }
+      }
+
+      $this->mail->smtpClose();
+      return $errors;
+    }
   }
-}
