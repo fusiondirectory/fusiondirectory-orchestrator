92fa6a0c9f644f464c23efdcd7ea1dd66c60b8d6
Thibault Dockx <thibault.dockx@fusiondirectory.org>
2024-08-15 14:39:06 +0100

    :sparkles: Feat(Audit) - first commit
    First audit commit logic as plugin

---
+++ plugins/tasks/Audit.php
@@ -0,0 +1,75 @@
+<?php
+
+class Audit implements EndpointInterface
+{
+
+  private TaskGateway $gateway;
+
+  public function __construct (TaskGateway $gateway)
+  {
+    $this->gateway = $gateway;
+  }
+
+  /**
+   * @return array
+   * Part of the interface of orchestrator plugin to treat GET method
+   */
+  public function processEndPointGet (): array
+  {
+    return [];
+  }
+
+  /**
+   * @param array|null $data
+   * @return array
+   */
+  public function processEndPointPost (array $data = NULL): array
+  {
+    return [];
+  }
+
+  /**
+   * @param array|NULL $data
+   * @return array
+   * @throws Exception
+   */
+  public function processEndPointPatch (array $data = NULL): array
+  {
+    return $this->processAuditDeletion($this->gateway->getObjectTypeTask('audit'));
+  }
+
+  /**
+   * @param array|NULL $data
+   * @return array
+   */
+  public function processEndPointDelete (array $data = NULL): array
+  {
+    return [];
+  }
+
+  /**
+   * @param array $auditSubTasks
+   * @return array
+   * @throws Exception
+   */
+  public function processAuditDeletion (array $auditSubTasks): array
+  {
+    $result = [];
+
+    // todo - Logic to iterate through audit timestamp and delete passed time.
+
+    return $result;
+  }
+
+  /**
+   * @param string $mainTaskDn
+   * @return array
+   */
+  public function getAuditMainTask (string $mainTaskDn): array
+  {
+    // Retrieve data from the main task
+    return $this->gateway->getLdapTasks('(objectClass=fdTasksNotifications)', ['attr1',
+      'attr2', 'attr3'], '', $mainTaskDn);
+  }
+
+}
