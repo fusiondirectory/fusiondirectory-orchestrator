e200e5ced3188d82f2a71120cb404f1b9f0e4ec4
Benoit Mortier <benoit.mortier@fusiondirectory.org>
2025-05-14 21:52:01 +0200

    :handshake: fix(jwt) remove the -jwt extension
    Signed-off-by: Benoit Mortier <benoit.mortier@fusiondirectory.org>

--- library/auth/UserGateway.php
+++ library/auth/UserGateway.php
@@ -12,9 +12,6 @@ class UserGateway
 
   public function authenticateDSA (string $dsaLogin, string $password): bool
   {
-    // Remove '-jwt' if present
-    $dsaLogin = str_replace('-jwt', '', $dsaLogin);
-
     // Construct DN directly (adjust as needed for your LDAP structure)
     $dn = "cn=$dsaLogin," . $_ENV["ORCHESTRATOR_ACCOUNT_BRANCH"];
 
@@ -29,7 +26,6 @@ class UserGateway
 
   public function getDSAInfo (string $dsaLogin): array
   {
-    $jwtCN  = $dsaLogin . "-jwt";
     $baseDN = $_ENV["ORCHESTRATOR_TOKEN_BRANCH"];
     $filter = "(cn=$jwtCN)";
     $attrs  = ["cn", "dn"];
