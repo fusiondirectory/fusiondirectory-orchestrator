1a4d7d23c147ce1a85743774cc415ac2f5183383
Benoit Mortier <benoit.mortier@fusiondirectory.org>
2025-05-14 22:18:03 +0200

    :handshake: fix(filter) make a better ldap filter for finding the token based on the objectclass
    Signed-off-by: Benoit Mortier <benoit.mortier@fusiondirectory.org>

--- library/auth/UserGateway.php
+++ library/auth/UserGateway.php
@@ -27,7 +27,7 @@ class UserGateway
   public function getDSAInfo (string $dsaLogin): array
   {
     $baseDN = $_ENV["ORCHESTRATOR_TOKEN_BRANCH"];
-    $filter = "(cn=$jwtCN)";
+    $filter = "(&(objectClass=fdJWT)(cn=$jwtCN))";
     $attrs  = ["cn", "dn"];
 
     $sr = @ldap_search($this->ds, $baseDN, $filter, $attrs);
