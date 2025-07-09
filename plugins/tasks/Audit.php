eed451b53ee5e5b4ec4260c30273aa7023a74f5f
Oana-Eliza Alexa <43857161+alexaeliza@users.noreply.github.com>
2025-07-09 12:23:31 +0300

    indent

--- plugins/tasks/Audit.php
+++ plugins/tasks/Audit.php
@@ -75,8 +75,10 @@ class Audit implements EndpointInterface
    */
   public function processAuditDeletion (array $auditSubTasks): array
   {
-     return array_values(array_map(fn($task) => $this->processScheduledTask($task),
-         array_filter($auditSubTasks, fn($task) => $this->gateway->statusAndScheduleCheck($task))));
+    return array_values(array_map(
+          fn($task) => $this->processScheduledTask($task),
+          array_filter($auditSubTasks, fn($task) => $this->gateway->statusAndScheduleCheck($task))
+    ));
   }
 
   /**
