b75581da42400de0999dad554ce7f6e341df9359
Oana-Eliza Alexa <43857161+alexaeliza@users.noreply.github.com>
2025-07-10 12:11:58 +0300

    phpdoc

--- plugins/tasks/AutomaticGroups.php
+++ plugins/tasks/AutomaticGroups.php
@@ -325,10 +325,10 @@ class AutomaticGroups implements EndpointInterface
     return $groupInfo[0]['member'] ?? [];
   }
 
-    /**
-     * @throws Exception
-     */
-    private function updateLdap (string $groupDn, string $userDn, array $entry, string $message): bool
+  /**
+   * @throws Exception
+  */
+  private function updateLdap (string $groupDn, string $userDn, array $entry, string $message): bool
   {
     // Update the group in LDAP
     try {
