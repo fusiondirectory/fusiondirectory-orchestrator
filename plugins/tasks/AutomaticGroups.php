28a18def93e67c862182da4cf30ed8ba93136c7b
Oana-Eliza Alexa <43857161+alexaeliza@users.noreply.github.com>
2025-07-14 13:01:30 +0300

    indent

--- plugins/tasks/AutomaticGroups.php
+++ plugins/tasks/AutomaticGroups.php
@@ -148,14 +148,14 @@ class AutomaticGroups implements EndpointInterface
 
   private function manageGroup (bool $shouldAddToGroup, string $userDn, string $targetGroup)
   {
-      if ($shouldAddToGroup) {
-          $this->addUserToGroup($userDn, $targetGroup);
-          $resultMessage[] = "User $userDn successfully added to group $targetGroup";
-      } else {
-          $this->removeUserFromGroup($userDn, $targetGroup);
-          $resultMessage[] = "User $userDn doesn't meet criteria - removed from group $targetGroup";
-      }
-      return $resultMessage;
+    if ($shouldAddToGroup) {
+      $this->addUserToGroup($userDn, $targetGroup);
+      $resultMessage[] = "User $userDn successfully added to group $targetGroup";
+    } else {
+      $this->removeUserFromGroup($userDn, $targetGroup);
+      $resultMessage[] = "User $userDn doesn't meet criteria - removed from group $targetGroup";
+    }
+    return $resultMessage;
   }
 
   /**
@@ -338,7 +338,7 @@ class AutomaticGroups implements EndpointInterface
     }
   }
 
-  private function getFailedMessage(string $userDn, string $message, string $groupDn): string
+  private function getFailedMessage (string $userDn, string $message, string $groupDn): string
   {
       return match ($message) {
           'create' => "Failed to create dynamic group: ",
@@ -347,14 +347,14 @@ class AutomaticGroups implements EndpointInterface
       };
   }
 
-    private function getErrorMessage(string $message): string
-    {
-        return match ($message) {
-            'create' => "Error creating dynamic group: ",
-            'add' => "Error adding member to group: ",
-            default => "Error removing member to group: ",
-        };
-    }
+  private function getErrorMessage (string $message): string
+  {
+    return match ($message) {
+      'create' => "Error creating dynamic group: ",
+      'add' => "Error adding member to group: ",
+      default => "Error removing member to group: ",
+    };
+  }
 
   /**
    * Add user to LDAP group
