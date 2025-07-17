fe682ea859762c6c67c6637f0a77fbe47594c803
Thibault Dockx <thibault.dockx@fusiondirectory.org>
2025-07-17 17:41:03 +0100

    :ambulance: Fixing a phpdoc issue for phpstan compliance

--- plugins/tasks/Reminder.php
+++ plugins/tasks/Reminder.php
@@ -520,7 +520,6 @@ class Reminder implements EndpointInterface
 
   /**
    * @param array $serverResults
-   * @param array $subTask
    * @param array $mailTaskBackend
    * @return array
    * Note : Process the mail response and update the task status with the main task DN and repeatable schedule
