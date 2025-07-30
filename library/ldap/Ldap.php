fa43868864f8cc4f4129272b811c01bee918c03a
Oana-Eliza Alexa <43857161+alexaeliza@users.noreply.github.com>
2025-07-30 14:27:38 +0300

    Make changes

--- library/ldap/Ldap.php
+++ library/ldap/Ldap.php
@@ -41,7 +41,7 @@ class Ldap
      * @return array
      * Note : A generic method allowing to search in LDAP.
      */
-  public function searchInLdap ($ds, string $filter = '', array $attrs = [], string $dn = NULL): array
+  public function searchInLdap (#[PhpVersionAware(['8.1' => 'LDAP\Connection'], default: 'resource')] $ds, string $filter = '', array $attrs = [], string $dn = NULL): array
   {
       $result = [];
 
