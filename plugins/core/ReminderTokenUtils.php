b7248ac06d2418d1f346c48939220e24f2b37e25
Jonathan Swaelens <jonathan.swaelens@fusiondirectory.org>
2026-02-10 10:01:29 +0000

    :ambulance: fix(phpstan): Fix Dead catch in ReminderTokenUtils.php
    Fix Dead catch in ReminderTokenUtils.php

--- plugins/core/ReminderTokenUtils.php
+++ plugins/core/ReminderTokenUtils.php
@@ -79,12 +79,6 @@ class ReminderTokenUtils
     $fdConfigAttributes  = $this->fdConfiguration->getFDConfigAttributes();
     $tokenBranch         = $fdConfigAttributes[0]['fdReminderTokenRDN'][0];
 
-    try {
-      $tokenBranch = $fdConfigAttributes[0]['fdReminderTokenRDN'][0];
-    } catch (Exception $e) {
-      throw new Exception("A branch is missing, please open your FusionDirectory configuration and save it.");
-    }
-
     // set the dn for the token, only take what's between "uid=" and ",ou="
     preg_match('/uid=([^,]+),ou=/', $userDN, $matches);
     $uid = $matches[1];
